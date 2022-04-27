babbef53ed10db7b696771d1b268c8d02f37d777
sredhamohan <sredhamohan91@gmail.com>
2022-04-27 12:29:08 +0530

    Framework related programs

--- src/Framework/Program3.java
+++ src/Framework/Program3.java
@@ -1,9 +1,12 @@
 package Framework;
 
+import java.io.File;
+
 public class Program3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		File f=new File(System.getProperty("user.dir") + "\\File\\Excel.xlsx");
 
 	}
 
